9a634096c29443503ba10d75c1da5aa621b7be3a
MrShubham-Sharma <s8974477@gmail.com>
2026-01-06 23:59:55 +0530

    Reset Hello.py to clean version

--- Hello.py
+++ Hello.py
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-print("HELLO MR.SHUBHAM")
-=======
-print("HELLO MR.SHUBHAM THIS IS MY FIRST STEP TOWARDS AI")
->>>>>>> d711793 (Day 7: Added site_manager and goal_check)
+print("HELLO MR.SHUBHAM THIS IS MY FIRST STEP TOWARDS AI/ML")
