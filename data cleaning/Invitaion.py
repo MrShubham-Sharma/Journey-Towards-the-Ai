04980d922c7161491fb97e9286c33e5bbadae529
MrShubham-Sharma <s8974477@gmail.com>
2026-01-17 00:28:27 +0530

    Day 9: Completed the loop control trifecta: break, continue, and while-remove

--- data cleaning/Invitaion.py
+++ data cleaning/Invitaion.py
@@ -1,5 +1,13 @@
+# The Scenario: You are making a list of friends to invite to a party, but you have had a fight with "Omkar". 
+# You want to print the invite for everyone EXCEPT him.
+
+# input
 friends = ["Rohit", "Omkar", "Shagun", "Parth"]
+
+# for loop for entry one by one 
 for name in friends:
+
+    # if the omkar found then continue will skip the omkar and continue the invitaion
     if name == 'Omkar':
         print("Skipping Omkar...")
         continue
