cc00a5e1a1f3962b162abcfad3132d25463b84b0
MrShubham-Sharma <s8974477@gmail.com>
2026-01-16 23:51:32 +0530

    Day 9: Implemented Data Cleaning logic using while loops and the in operator

--- data cleaning/broken_Bricks.py
+++ data cleaning/broken_Bricks.py
@@ -1,5 +1,12 @@
+# the input initialization
 Bricks = ['Red','Black','Red','Red','Black','Black','Red']
+
+# While loop for the Bricks 
 while 'Black' in Bricks:
     print("The Bad Quality Of Brick Found! We Removed It ")
+    
+    # remove.('black') removes the bad brick and clean data 
     Bricks.remove('Black')
+
+# the final output
 print(f"The God Quality Bricks are {Bricks}")
