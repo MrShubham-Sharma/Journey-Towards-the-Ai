b4fcce4875946e2fe5e29e13e549f386a09d6070
MrShubham-Sharma <s8974477@gmail.com>
2026-01-20 00:48:41 +0530

    Day 11: Visualized nested loop execution flow with student grading system

--- Nested loops/marks_system.py
+++ Nested loops/marks_system.py
@@ -1,6 +1,17 @@
+# the Data of student name 
 for students in ["Omkar", "Shagun"]:
+
+# take the student name from list from outer loop 
     print(f"the name of Student is :{students} ")
+
+    # the inner loop which have subject 
     for subjects in ["Math", "Python"]:
+
+        # the score input of student's subject
         score = input(f"Enter {students}'s marks {subjects}: ")
+
+        # conformation of record of score and student 
         print(f"The Recorded {subjects}= {score}: ")
+
+# final output
 print("\n All grades are logged in the system! ")
