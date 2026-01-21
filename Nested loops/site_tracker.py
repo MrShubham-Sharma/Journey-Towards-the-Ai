17b9ab5f3eb2223473e7d4565be9debb6b5dea64
MrShubham-Sharma <s8974477@gmail.com>
2026-01-21 18:17:18 +0530

    Day 13: Fixed variable scope bug and implemented nested accumulator logic

--- Nested loops/site_tracker.py
+++ Nested loops/site_tracker.py
@@ -1,13 +1,19 @@
+# for reset grand total 
+grand_total = 0
+
+# outer loop 
 for days in ["Monday", "Tuesday"]:
-    daily_total = 0  #This is the "Reset" we discussed!
-    print(f"--- Processing: {days} ---")
+    daily_total = 0  #This is the "Reset"
+    print(f"Processing: {days} ")
     
+    # inner lopp
     for materials in ["Cement", "Steel"]:
-        grand_total = 0
         cost = int(input(f"Enter cost for {materials} on {days}: "))
         daily_total += cost
         grand_total += cost
-        
+
+        # final verification
     print(f"Total spent on {days}: {daily_total}")
 
+# grand total
 print(f"\n GRAND TOTAL for the site: {grand_total}")
