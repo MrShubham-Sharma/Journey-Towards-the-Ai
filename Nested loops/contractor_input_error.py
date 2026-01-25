0dc2cbfadfa297049b9de55f702172f81843c042
MrShubham-Sharma <s8974477@gmail.com>
2026-01-25 23:48:41 +0530

    Day 15: devloped try and expect condtional error in real life

---
+++ Nested loops/contractor_input_error.py
@@ -0,0 +1,25 @@
+# the value Resetor for tatal value 
+grand_total =0
+
+# inner loop for the Rooms
+for Room in ["Kitchen","Hall","Bedroom"]:
+
+    # vlaue restor for the Room Total
+    room_total = 0
+    print(f"the budget of the {Room}")
+
+    # Inner loop for the Material
+    for Material in ["Cement","Sand","Bricks"]:
+        
+        # the condition for the input 
+        try:
+            cost = int(input(f" The Budget of the {Room} is :"))
+            grand_total += cost
+            room_total += cost
+
+        #if the input is wrong or get skipped then it will show tis error meassege  
+        except:
+              print(f"Unexpected eroor you try variable / you skip the input")
+    
+    print(f"The cost of the {Room} is Rs.{room_total} ")
+print(f"The Final Budget Of Project is Rs.{grand_total} ")
