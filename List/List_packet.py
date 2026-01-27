d5f542c293bbd58fe06849c974edb96b94dca20f
MrShubham-Sharma <s8974477@gmail.com>
2026-01-27 22:45:15 +0530

    Day 17: Understanding How List Works

---
+++ List/List_packet.py
@@ -0,0 +1,10 @@
+# Creating the List
+packing_list = ["Trekking Shoes", "Heavy Jacket", "Power Bank", "Camera"]
+
+# Accessing one item (Python starts counting at 0!)
+print(packing_list[0])  # This prints: Trekking Shoes
+
+# Adding an item to the "Backpack"
+packing_list.append("Gloves")
+
+print(f"I am taking {len(packing_list)} items to Darjeeling.")
