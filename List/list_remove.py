36ef2b481904e531b50e124ed87106e5d5d05133
MrShubham-Sharma <s8974477@gmail.com>
2026-01-27 23:15:18 +0530

    Day:17 Learn How to use .remove()

---
+++ List/list_remove.py
@@ -0,0 +1,10 @@
+# created an list 
+friends_group = ["Rohit","Parth","Shagun","Pratik"]
+print(friends_group[0])
+
+# used the .remove() opraton
+friends_group.remove("Shagun")
+
+# use the .join for clen output
+perfect_names = ",".join(friends_group)
+print(f"{perfect_names} These are My Niphad's Friends")
