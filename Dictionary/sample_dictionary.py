0033f90a24152ebdb4597a8b2805f7d09613c55c
MrShubham-Sharma <s8974477@gmail.com>
2026-01-28 20:51:10 +0530

    Day 18:Learning Basic Structure Of Dictionary

---
+++ Dictionary/sample_dictionary.py
@@ -0,0 +1,15 @@
+# 1. Creating the Dictionary {Key: Value}
+trip_budget = {
+    "Train": 1500,
+    "Hotel": 3000,
+    "Food": 2000,
+    "Sightseeing": 500
+}
+
+# 2. Accessing data by its Label (Key)
+print(trip_budget["Hotel"])  # Prints: 3000
+
+# 3. Adding a new item
+trip_budget["Shopping"] = 1000
+
+print(trip_budget)
