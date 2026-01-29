7d72abb65332501c0fca98e988cbb313cd3fe17b
MrShubham-Sharma <s8974477@gmail.com>
2026-01-29 21:46:49 +0530

    Dat 19: learn how the :.2f works

---
+++ Function/use_of_.2f.py
@@ -0,0 +1,9 @@
+avg = 6.008000000000001
+
+print(f"Messy: {avg}")
+print(f"Clean: {avg:.2f}") # Rounds the 0 to 1 because the next digit is 8!
+
+cement_price = 450.789
+quantity = 15
+total = cement_price * quantity
+print(f"the Total price is {total:.2f}")
