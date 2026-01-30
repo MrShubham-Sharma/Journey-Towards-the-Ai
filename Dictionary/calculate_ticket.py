f353874ceec8bd51fe2642f8265757b17e3def26
MrShubham-Sharma <s8974477@gmail.com>
2026-01-30 15:38:50 +0530

    Day 20: adding 3rd sinario for ticket

--- Dictionary/calculate_ticket.py
+++ Dictionary/calculate_ticket.py
@@ -1,19 +1,24 @@
-# 'def' creates the machine. 
-# It needs two 'ingredients': price and is_student (True or False)
-def calculate_ticket(price, is_student):
-    if is_student == True:
-        discount = price * 0.40  # 40% Discount
-        final_price = price - discount
-        return final_price       # Handing the discounted price back
+# FIX 1: Add '=False' to make these optional. This is a "Default Parameter."
+def calculate_ticket(price, is_student=False, is_senior_citizen=False):
+    if is_student:
+        discount = price * 0.40
+        return price - discount
+    elif is_senior_citizen:
+        discount = price * 0.60
+        return price - discount
     else:
-        return price             # Handing the full price back
+        return price
 
-# USING THE MACHINE
+# --- USING THE MACHINE ---
 
-# Scenario 1: You (A Student) traveling on Tapovan
-my_ticket = calculate_ticket(500, True) 
+# Scenario 1: Providing the value 'True' for student
+my_ticket = calculate_ticket(500, is_student=True) 
 print(f"Shubham's Student Ticket: Rs.{my_ticket:.2f}")
 
-# Scenario 2: A regular passenger
+# Scenario 2: Providing 'False' for student, and it assumes 'False' for senior
 normal_ticket = calculate_ticket(500, False)
 print(f"Regular Ticket: Rs.{normal_ticket:.2f}")
+
+# Scenario 3: Specifically telling it Senior is 'True'
+old_age_ticket = calculate_ticket(500, is_senior_citizen=True)
+print(f"Senior Citizen Ticket: Rs.{old_age_ticket:.2f}")
