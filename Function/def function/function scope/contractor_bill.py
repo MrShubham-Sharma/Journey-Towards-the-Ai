1a53c226b8414ade11c796ddca2fa11c191108a4
MrShubham-Sharma <s8974477@gmail.com>
2026-02-03 00:24:38 +0530

    Day 22: convert the simple bill genrator into smart one

--- Function/def function/function scope/contractor_bill.py
+++ Function/def function/function scope/contractor_bill.py
@@ -5,3 +5,10 @@ def Bill_of_Material(cement, sand, transport):
     return total_ammount
 total_bill = Bill_of_Material(800,700,300)
 print(f"the total Bill of the Day is Rs.{total_bill}")
+
+def smart_bill(material_cost,Tax_persentage):
+    Tax_ammount = material_cost * Tax_persentage / 100
+    total_cost = material_cost + Tax_ammount
+    return total_cost
+bill_with_tax = smart_bill(400,18)
+print(f"The Total Cost MRP.Rs.{bill_with_tax:.2f}")
