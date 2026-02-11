8b9c8dc68716ffab722cd266efd7f081e5b5af22
MrShubham-Sharma <s8974477@gmail.com>
2026-02-12 00:11:55 +0530

    Day 27:Test the Bill Program

---
+++ Function/def function/function scope/bill_scope.py
@@ -0,0 +1,17 @@
+def calculate_chai_bill():
+    chai_price = 20 # This is LOCAL
+    return f"Chai is {chai_price}"
+
+calculate_chai_bill()
+
+# HR QUESTION: "Can I print chai_price here?"
+print(chai_price) #it will show the name error 
+
+# corrected code 
+def calculate_chai_bill():
+    price = 20
+    return price # Sending the value across the bridge
+
+# We catch it in a GLOBAL variable
+final_price = calculate_chai_bill() 
+print(f"I can see it now: {final_price}")
