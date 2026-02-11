e192270fb310395ddc1ed485f325b504c177797c
MrShubham-Sharma <s8974477@gmail.com>
2026-02-12 00:41:30 +0530

    Day 28: learn How to Use import library in Pro way

---
+++ Toolbox Import/import_randit.py
@@ -0,0 +1,7 @@
+from random import randint # Just grab the one tool
+
+def surprise_cost():
+    # Now you don't need to type "random." anymore!
+    return randint(10, 100) 
+
+print(surprise_cost())
