55dda8ff7fb9025bd44c6a2176dd205d9aa6f471
MrShubham-Sharma <s8974477@gmail.com>
2026-02-14 01:26:23 +0530

    Learn The [-3:]

--- String/Slicing/Reverse.py
+++ String/Slicing/Reverse.py
@@ -2,6 +2,8 @@ Name = "SHUBHAM"
 print(Name[0:4])   #It Will Print SHUBH
 print(Name[::-1])  #It Will Print MAHBUHS
 print(Name[3:])    #It Will Print BHAM
+print(Name[-3:])   #It will Print HAM
+
 
 score = "SGPA:6.74"
 print(score[5:])   #It Will Print 6.74
