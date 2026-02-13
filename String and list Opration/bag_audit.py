4c86b6213e8b008d596c55df8c1fd733780690af
MrShubham-Sharma <s8974477@gmail.com>
2026-02-14 03:52:48 +0530

    Final Audit Program Of day

---
+++ String and list Opration/bag_audit.py
@@ -0,0 +1,5 @@
+audit_bag = {"Cement", "Sand", "Bricks", "Cement"}  #It Will Remove The Duplicate Data In set
+print(audit_bag)
+print("Cement"in audit_bag)                         #checks The Cement in a bag 
+cement_bag = 10000//1500                            #give the Count of How many Bags Can be purchased in Rs.10000
+print(f"you can perchase {cement_bag} Cement Bags In Rs.10000")
