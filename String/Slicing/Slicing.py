1aca89d513c089e066e7c1b1f62038fd8b26e7e2
MrShubham-Sharma <s8974477@gmail.com>
2026-02-14 01:52:40 +0530

    Learn the use case upper lower title capitalize swapcase

--- String/Slicing/Slicing.py
+++ String/Slicing/Slicing.py
@@ -1,9 +1,18 @@
-Name = "SHUBHAM"
+Name = "SHUBhAM"
 print(Name[0:4])   #It Will Print SHUBH
 print(Name[::-1])  #It Will Print MAHBUHS
 print(Name[3:])    #It Will Print BHAM
 print(Name[-3:])   #It will Print HAM
 
+# Use Cases
+Name.upper()
+print(Name.upper())
+Name.lower()
+print(Name.lower())
+Name.title()
+print(Name.title())
+print(Name.capitalize())
+print(Name.swapcase())
 
 score = "SGPA:6.74"
 print(score[5:])   #It Will Print 6.74
