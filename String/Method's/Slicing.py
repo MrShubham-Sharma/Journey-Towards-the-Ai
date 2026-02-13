fc5506d4bfc4adf1ee964aab705dbe7c370f7cd6
MrShubham-Sharma <s8974477@gmail.com>
2026-02-14 02:02:36 +0530

    Learn The strip

---
+++ String/Method's/Slicing.py
@@ -0,0 +1,16 @@
+Name = "SHUBhAM"
+print(Name[0:4])   #It Will Print SHUBH
+print(Name[::-1])  #It Will Print MAHBUHS
+print(Name[3:])    #It Will Print BHAM
+print(Name[-3:])   #It will Print HAM
+
+# Use Cases
+print(Name.upper())          #It Will Print SHUBHAM
+print(Name.lower())          #It Will Print shubham
+print(Name.title())          #It Will Print Shubham
+print(Name.capitalize())     #It Will Print Shubham
+print(Name.swapcase())       #It Will Print shubHam
+
+score = " SGPA:6.74 "
+print(score[5:])     #It Will Print 6.74
+print(score.strip()) #It Will Print SGPA:6.74
