618c089ed6e345d2b3e1138c373c433b32a64f38
MrShubham-Sharma <s8974477@gmail.com>
2026-02-14 01:23:06 +0530

    Learn The the Slicing in String

---
+++ String/Slicing/Reverse.py
@@ -0,0 +1,7 @@
+Name = "SHUBHAM"
+print(Name[0:4])   #It Will Print SHUBH
+print(Name[::-1])  #It Will Print MAHBUHS
+print(Name[3:])    #It Will Print BHAM
+
+score = "SGPA:6.74"
+print(score[5:])   #It Will Print 6.74
