e0adca344bec582fcda2827efc5458125527bf1f
MrShubham-Sharma <s8974477@gmail.com>
2026-02-18 22:55:31 +0530

    Added data in site audit.txt file

--- File Handling/Append.py
+++ File Handling/Append.py
@@ -4,3 +4,8 @@ site_file = open("Site Audit.txt", "a")
 site_file.write("Bricks = 4000.Rs\n")
 
 site_file.close()
+
+site_file = open("Site Audit.txt", "a") 
+site_file.write("Bricks = 4000.Rs\n")
+site_file.write("The Cost Of Today Is 4000.Rs\n")
+site_file.close()
