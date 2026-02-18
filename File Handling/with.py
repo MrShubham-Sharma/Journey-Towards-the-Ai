8957b489c3d996fabacb58ac5dca575b5d11e63c
MrShubham-Sharma <s8974477@gmail.com>
2026-02-18 23:19:51 +0530

    Learn to use With open()

---
+++ File Handling/with.py
@@ -0,0 +1,5 @@
+with open("Hostel Audit.txt", "a") as site_file:
+    site_file.write("Rent = 4000.Rs\n")
+    site_file.write("Food = 2000.Rs")
+    site_file.write("The Cost Of Today Is 6000.Rs\n")
+# No .close() needed! Python does it for you here.
