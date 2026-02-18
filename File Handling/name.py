3c2a0105f8e4469ef49cab334787351230c39eb7
MrShubham-Sharma <s8974477@gmail.com>
2026-02-18 22:33:00 +0530

    Added Some Changes

--- File Handling/name.py
+++ File Handling/name.py
@@ -1,3 +1,9 @@
-file=open("Name.txt", "w")
-file.write("The Owenre is Shubham")
-file.close()
+# open() will file my_file.txt 
+# w stands for for Write. It tells Python: "I want to write something new. 
+# If this file already exists, delete everything inside and let me start fresh
+my_file=open("Owner.txt", "w")
+
+# write() stands for Writing/Overrding 
+my_file.write("The Owenris Shubham")
+my_file.write("The Goal Is to Become an Ai/ML Engineer")
+my_file.close()
