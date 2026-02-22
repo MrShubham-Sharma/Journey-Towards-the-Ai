d88ab64a735c5a13a2c19bf134e6f8f212393839
MrShubham-Sharma <s8974477@gmail.com>
2026-02-22 22:29:23 +0530

    Practice code

---
+++ String and list Opration/Practice3.py
@@ -0,0 +1,13 @@
+# 1. SET: Removing duplicates from a messy audit list
+audit_items = {"Cement", "Sand", "Bricks", "Cement", "Sand"}
+print(f"Unique Items for Audit: {audit_items}")
+
+# 2. TUPLE: Storing fixed data that shouldn't change
+college_info = ("Shree L. R. Tiwari", "Mira Road", "IT Engineering")
+
+# 3. MATH: Using Floor Division (//) to calculate whole units
+total_budget = 10000
+cement_cost = 1500
+full_bags = total_budget // cement_cost
+
+print(f"At {college_info[0]}, we calculated {full_bags} bags can be bought.")
