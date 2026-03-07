36f5c9dfc514a934583212439f6472e13f40bbde
MrShubham-Sharma <s8974477@gmail.com>
2026-03-08 00:43:23 +0530

    Revised oprators

---
+++ Revised Topics/Floor_revised.py
@@ -0,0 +1,6 @@
+total_budget = 25000
+Single_Room_cost = 3200
+Rooms_We_can_Book = total_budget //Single_Room_cost
+The_moany_left = total_budget %Single_Room_cost
+print(f"We Can Book the :{Rooms_We_can_Book}")
+print(f"The Moany Will Be left :{The_moany_left}")
