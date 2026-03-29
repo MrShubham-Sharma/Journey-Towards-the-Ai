f3c99b7ba464015fa25e624a99abc48ac2d82ccb
MrShubham-Sharma <s8974477@gmail.com>
2026-03-29 23:49:28 +0530

    Slove Simple Problem

---
+++ Assingment Oprator/atithmatic.py
@@ -0,0 +1,7 @@
+base_price = 100
+tax_rate = 0.05
+
+# Assign the result of a calculation to a new variable
+total_price = base_price + (base_price * tax_rate)
+
+print(f"The total price is: {total_price}")
