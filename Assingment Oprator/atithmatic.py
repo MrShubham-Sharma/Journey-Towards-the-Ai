02564ae269dc4dc4b30d2ca61bf501a62f7faad9
MrShubham-Sharma <s8974477@gmail.com>
2026-03-29 23:50:18 +0530

    Added one more program

--- Assingment Oprator/atithmatic.py
+++ Assingment Oprator/atithmatic.py
@@ -5,3 +5,15 @@ tax_rate = 0.05
 total_price = base_price + (base_price * tax_rate)
 
 print(f"The total price is: {total_price}")
+
+# Starting balance
+account_balance = 500.00
+
+# Using augmented assignment operators to update the value
+deposit = 150.0
+account_balance += deposit  # Equivalent to: account_balance = account_balance + deposit
+
+withdrawal = 50.0
+account_balance -= withdrawal  # Equivalent to: account_balance = account_balance - withdrawal
+
+print(f"Updated Balance: ${account_balance}")
