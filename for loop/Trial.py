d4f827f4f800126c11033cb229cfa6d4bace0764
MrShubham-Sharma <s8974477@gmail.com>
2026-03-30 23:22:06 +0530

    Welcome

---
+++ for loop/Trial.py
@@ -0,0 +1,10 @@
+Name = ["Shubham","Rohit","Parth"]
+for i in Name :
+    if i == "Shubham":
+        print("Hello Master Welcome Back")
+    elif i == "Rohit":
+        print("Hello Buddy")
+    elif i =="Parth":
+        print("Hello My Nigga")
+    else:
+        print("Error Unkown User")
