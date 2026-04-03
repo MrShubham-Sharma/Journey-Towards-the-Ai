4c9b1458ffbcd9724e218bc67650f4e659a0ab59
Mr.Shubham-Sharma <s8974477@gmail.com>
2026-04-03 19:09:46 +0530

    Added Number program

---
+++ for loop/itration.py
@@ -0,0 +1,12 @@
+# A list of different fruits
+fruits = ["apple", "banana", "cherry"]
+
+# The for loop iterates over each item in the list
+for fruit in fruits:
+    print(f"I like {fruit}s!")
+
+# The range(1, 6) generates numbers from 1 up to (but not including) 6
+for number in range(1, 6):
+    # This calculates the square of the current number
+    square = number * number
+    print(f"The square of {number} is {square}")
