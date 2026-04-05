0f86f0d22a199a1dc1b9f45e547aea492f1b3357
Mr.Shubham-Sharma <s8974477@gmail.com>
2026-04-05 20:29:51 +0530

    revised fucntion for loop

--- for loop/Range.py
+++ for loop/Range.py
@@ -4,6 +4,10 @@
 # for i in range(1,6):
 #     print(i)
 # print("Done!")
-for i in range(1,11,5):
+# for i in range(1,5,2):
+#     print(i)
+# print("DONE!")
+# for i in range(5,0,-1):
+#     print(i)
+for i in reversed(range(1,10,-1)):
     print(i)
-print("DONE!")
