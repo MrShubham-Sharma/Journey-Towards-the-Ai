d4d0a6dbbe2a0a999317e0a5e5e0d053c6644697
Mr.Shubham-Sharma <s8974477@gmail.com>
2026-04-06 23:50:26 +0530

    Revise Sclicing Opration

--- for loop/Range.py
+++ for loop/Range.py
@@ -1,17 +1,20 @@
-# for i in range(5):
-#     print(i)
-# print("done!")
-# for i in range(1,6):
-#     print(i)
-# print("Done!")
-# for i in range(1,5,2):
-#     print(i)
-# print("DONE!")
-# for i in range(5,0,-1):
-#     print(i)
-# for i in reversed(range(1,10,1)):
-#     print(i)
+for i in range(5):
+    print(i)
+print("done!")
+for i in range(1,6):
+    print(i)
+print("Done!")
+for i in range(1,5,2):
+    print(i)
+print("DONE!")
+for i in range(5,0,-1):
+    print(i)
+for i in reversed(range(1,10,1)):
+    print(i)
 
 name = "Shubham"
+print(name[::-1])
 for i in name:
     print(i ,end='')
+for i in name[::-1]:
+    print(i,end=''/n)
