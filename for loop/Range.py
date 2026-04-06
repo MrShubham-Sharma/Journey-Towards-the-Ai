3df05402ada2f6d458f65818588102b540e8978d
Mr.Shubham-Sharma <s8974477@gmail.com>
2026-04-06 23:56:28 +0530

    Revise count and f string Opration

--- for loop/Range.py
+++ for loop/Range.py
@@ -1,20 +1,26 @@
-for i in range(5):
-    print(i)
-print("done!")
-for i in range(1,6):
-    print(i)
-print("Done!")
-for i in range(1,5,2):
-    print(i)
-print("DONE!")
-for i in range(5,0,-1):
-    print(i)
-for i in reversed(range(1,10,1)):
-    print(i)
+# for i in range(5):
+#     print(i)
+# print("done!")
+# for i in range(1,6):
+#     print(i)
+# print("Done!")
+# for i in range(1,5,2):
+#     print(i)
+# print("DONE!")
+# for i in range(5,0,-1):
+#     print(i)
+# for i in reversed(range(1,10,1)):
+#     print(i)
 
-name = "Shubham"
-print(name[::-1])
-for i in name:
-    print(i ,end='')
-for i in name[::-1]:
-    print(i,end=''/n)
+# name = "Shubham"
+# print(name[::-1])
+# for i in name:
+#     print(i ,end='')
+# for i in name[::-1]:
+#     print(i,end='')
+
+Sentence = "Hello My NIGGA HOW ARE YOU"
+count=0
+for N in Sentence.split():
+    count +=1
+print(f"There are {count} of words in sentence.")
