0c88e7489004a0d6562654d5f1e1f00422dd9ace
Mr.Shubham-Sharma <s8974477@gmail.com>
2026-04-09 00:08:34 +0530

    Change File Name By Break And Continue

--- Termination Break And Continue/Break_AND_continue.py
+++ Termination Break And Continue/Break_AND_continue.py
@@ -1,22 +1,22 @@
 # Break
-# numbers = list(range(0,100))
-# for number in numbers:
-#     if number > 20:
-#         break
-#     print(number)
-# while True:
-#     num =input("Enter The Number (q For Quite) :")
-#     # if we enter q it will quite loop
-#     if num =='q' :
-#         break
-#     print(num)
+numbers = list(range(0,100))
+for number in numbers:
+    if number > 20:
+        break
+    print(number)
+while True:
+    num =input("Enter The Number (q For Quite) :")
+    # if we enter q it will quite loop
+    if num =='q' :
+        break
+    print(num)
 
 # continue
 # we have to Skip 2 and 4
-# for i in range(5):
-#     if i == 2 or i == 4:
-#         continue
-#     print(i)
+for i in range(5):
+    if i == 2 or i == 4:
+        continue
+    print(i)
 
 # Now We Have to Print 2,4,6,8
 n=0
