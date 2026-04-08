51fa29089dcb643166bc7eb0996162f956947d26
Mr.Shubham-Sharma <s8974477@gmail.com>
2026-04-08 23:42:58 +0530

    Revise Continue opration

--- Termination Break And Continue/Break.py
+++ Termination Break And Continue/Break.py
@@ -1,10 +1,18 @@
+# Break
 # numbers = list(range(0,100))
 # for number in numbers:
 #     if number > 20:
 #         break
 #     print(number)
-while True:
-    num =input("Enter The Number (q For Quit) :")
-    if num =='q' :
-        break
-    print(num)
+# while True:
+#     num =input("Enter The Number (q For Quite) :")
+#     # if we enter q it will quite loop
+#     if num =='q' :
+#         break
+#     print(num)
+
+# continue
+for i in range(5):
+    if i == 2 or i == 4:
+        continue
+    print(i)
