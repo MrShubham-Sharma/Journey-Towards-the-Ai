6bbae2dabbcd5b76792f96c00870010d6007cebe
Mr.Shubham-Sharma <s8974477@gmail.com>
2026-04-08 23:31:26 +0530

    Revse while loop Logic with Break

--- Termination Break And Continue/Break.py
+++ Termination Break And Continue/Break.py
@@ -1,5 +1,10 @@
-numbers = list(range(0,100))
-for number in numbers:
-    if number > 20:
+# numbers = list(range(0,100))
+# for number in numbers:
+#     if number > 20:
+#         break
+#     print(number)
+while True:
+    num =input("Enter The Number (q For Quit) :")
+    if num =='q' :
         break
-    print(number)
+    print(num)
