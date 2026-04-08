370c163bcdcb6ad2db0a41ec51f3384a3af1de58
Mr.Shubham-Sharma <s8974477@gmail.com>
2026-04-08 23:26:34 +0530

    Revise Break Condition

---
+++ Termination Break And Continue/Break.py
@@ -0,0 +1,5 @@
+numbers = list(range(0,100))
+for number in numbers:
+    if number > 20:
+        break
+    print(number)
--- for loop/Range.py
+++ for loop/Range.py
@@ -1,36 +1,36 @@
-# for i in range(5):
-#     print(i)
-# print("done!")
-# for i in range(1,6):
-#     print(i)
-# print("Done!")
-# for i in range(1,5,2):
-#     print(i)
-# print("DONE!")
-# for i in range(5,0,-1):
-#     print(i)
-# for i in reversed(range(1,10,1)):
-#     print(i)
+for i in range(5):
+    print(i)
+print("done!")
+for i in range(1,6):
+    print(i)
+print("Done!")
+for i in range(1,5,2):
+    print(i)
+print("DONE!")
+for i in range(5,0,-1):
+    print(i)
+for i in reversed(range(1,10,1)):
+    print(i)
 
-# name = "Shubham"
-# print(name[::-1])
-# for i in name:
-#     print(i ,end='')
-# for i in name[::-1]:
-#     print(i,end='')
+name = "Shubham"
+print(name[::-1])
+for i in name:
+    print(i ,end='')
+for i in name[::-1]:
+    print(i,end='')
 
-# Sentence = "Hello My NIGGA HOW ARE YOU"
-# count=0
-# for N in Sentence.split():
-#     count +=1
-# print(f"There are {count} of words in sentence.")
-# # List Opation 
-# Cars = ["BMW","AUDI","TOYOTA"]
-# for car in Cars:
-#     print(car)
+Sentence = "Hello My NIGGA HOW ARE YOU"
+count=0
+for N in Sentence.split():
+    count +=1
+print(f"There are {count} of words in sentence.")
+# List Opation 
+Cars = ["BMW","AUDI","TOYOTA"]
+for car in Cars:
+    print(car)
 
-# Cars = ["BMW","AUDI","TOYOTA"]
-# for car in range(len(Cars)):
-#     print(Cars[car])
+Cars = ["BMW","AUDI","TOYOTA"]
+for car in range(len(Cars)):
+    print(Cars[car])
 Cars = ["BMW","AUDI","TOYOTA"]
 [print(car)for car in Cars]
