2904ef007af457a8d726e8b992aba51b6ee94585
Mr.Shubham-Sharma <s8974477@gmail.com>
2026-04-25 23:37:46 +0530

    Revised Use Of Read()

--- File Handling/Hello.py
+++ File Handling/Hello.py
@@ -1,4 +1,14 @@
+# Writing To The File
 User = open("Hello.txt","w")
 User.write("Hello Master\n")
 User.write("Welcome To Python Programming\n")
 User.close()
+
+# Reading The File
+with open("Hello.txt", "r") as user_file:
+    content = user_file.read()
+    print(content)
+
+# Appending To The File
+with open("Hello.txt", "a") as user_file:
+    user_file.write("Have A Nice Day\n")
