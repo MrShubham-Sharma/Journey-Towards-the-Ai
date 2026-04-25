8fe08b1a17d45bfc29564cdb16d2422937b61316
Mr.Shubham-Sharma <s8974477@gmail.com>
2026-04-25 23:55:26 +0530

    Try Some oprations r, a & w

--- File Handling/Hello.py
+++ File Handling/Hello.py
@@ -11,4 +11,17 @@ with open("Hello.txt", "r") as user_file:
 
 # Appending To The File
 with open("Hello.txt","a") as user_file:
-    user_file.write("This Line Is Appended\n")
+    user_file.write("Hello Brother\n")
+
+Hello = open("Hello.txt","r")
+print(Hello.read())
+Hello.close()
+
+with open("Helllo.txt","w") as user_file:
+    user_file.write("Hello Master Shubh\n")
+    user_file.close()
+
+Hello = open("Helllo.txt","r")
+print(Hello.read())
+Hello.close()
+Hello.close()
