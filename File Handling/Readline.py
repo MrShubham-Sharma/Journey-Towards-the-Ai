e027c16c205617ec971ab9dbd33e9c618802d2d0
Mr.Shubham-Sharma <s8974477@gmail.com>
2026-04-26 23:36:56 +0530

    Learn Structure For readline()

--- File Handling/Readline.py
+++ File Handling/Readline.py
@@ -2,5 +2,13 @@ with open("shubh.txt","w") as My_file:
     My_file.write("Welcome Back Master\n")
 
 with open("shubh.txt","r") as My_file:
-    My_file.read()
-    print(My_file)
+    content = My_file.read()
+    print(content)
+
+# readline()
+f = open ("shubh.txt","r")
+while True:
+    line = f.readline()
+    if not line:
+        break
+    print(line)
