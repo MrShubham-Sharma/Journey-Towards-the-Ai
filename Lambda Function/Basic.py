293b3326256575f70336401c4b6fb5399f54d4e5
Mr.Shubham-Sharma <s8974477@gmail.com>
2026-04-28 23:12:54 +0530

    Learnig Lambda Function

---
+++ Lambda Function/Basic.py
@@ -0,0 +1,5 @@
+# lambda function 
+# lambda function is a small anonymous function that can take any number of arguments, 
+# but can only have one expression.
+double = lambda x: x * 2
+print(double(5)) 
